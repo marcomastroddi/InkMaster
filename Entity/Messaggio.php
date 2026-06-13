c84bdd266b5de11ecb07688db8f09b4af5c4e054
Marco Mastroddi <marcomastroddi1@gmail.com>
2026-06-13 12:10:07 +0200

    V1.17 Aggiunta classe Messaggio, con anche inserimento nuovo attributo,

---
+++ Entity/Messaggio.php
@@ -0,0 +1,10 @@
+<?php
+class Messaggio { 
+    private $id;  
+    private $mittente;
+    private $destinatario;
+    private $testo;
+    private $data_invio;
+    private $ora_invio;
+    private $immagine;    
+}
