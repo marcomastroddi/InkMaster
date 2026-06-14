8547a234e100c3134047f643c5fbc1989517a6de
lorenzolika29 <lorenzolika29@gmail.com>
2026-06-14 16:27:51 +0200

    Configurato Composer e aggiunto Doctrine all'ambiente

--- Entity/Appuntamento.php
+++ Entity/Appuntamento.php
@@ -1,8 +1,37 @@
 <?php
+
 class Appuntamento {
+    private $id;
     private $data;
-    private $stato;
-    private $note;
-    private $ora_inizio;
-    private $ora_fine;
+    private $ora;
+    private $cliente;
+    private $tatuatore;
+
+    public function __construct($id, $data, $ora, $cliente, $tatuatore) {
+        $this->id = $id;
+        $this->data = $data;
+        $this->ora = $ora;
+        $this->cliente = $cliente;
+        $this->tatuatore = $tatuatore;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function getOra() {
+        return $this->ora;
+    }
+
+    public function getCliente() {
+        return $this->cliente;
+    }
+
+    public function getTatuatore() {
+        return $this->tatuatore;
+    }
 }
