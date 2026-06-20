fa284c7eb7744001f9bacbcee6d60cbe3dca6866
Marco Mastroddi <marcomastroddi1@gmail.com>
2026-06-20 12:00:45 +0200

    Correzione USC4

---
+++ src/Foundation/RecensioneRepository.php
@@ -0,0 +1,47 @@
+<?php
+namespace InkMaster\Foundation;
+
+use InkMaster\Entity\Recensione;
+use InkMaster\Entity\Cliente;
+use InkMaster\Entity\Studio;
+use InkMaster\Entity\Tatuatore;
+use DateTime;
+
+class RecensioneRepository
+{
+    private $em;
+
+    public function __construct($entityManager = null)
+    {
+        $this->em = $entityManager;
+    }
+
+    public function salvaRecensione(
+        int $voto,
+        string $titolo,
+        string $descrizione,
+        ?string $foto,
+        string $stile,
+        Cliente $cliente,
+        Studio $studio,
+        Tatuatore $tatuatore
+    ): Recensione {
+        $recensione = new Recensione(
+            $voto,
+            new DateTime(),
+            $cliente,
+            $studio,
+            $titolo,
+            $stile,
+            $tatuatore,
+            $descrizione,
+            $foto
+        );
+
+        // da implementare con il db:
+        // $this->em->persist($recensione);
+        // $this->em->flush();
+
+        return $recensione;
+    }
+}
