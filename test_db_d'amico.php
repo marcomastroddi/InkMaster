b9a5b7dc0b9e40fab06e77284b83f32584d2b69d
Marco Mastroddi <marcomastroddi1@gmail.com>
2026-06-23 00:40:55 +0200

    db in PrenotazionePagamento

--- test_db_d'amico.php
+++ test_db_d'amico.php
@@ -5,11 +5,6 @@ use InkMaster\Foundation\PersistentManager;
 
 $pm = PersistentManager::getInstance();
 
-echo "=== STILI DAL DATABASE ===\n";
-foreach ($pm->findAvailableStyles() as $stile) {
-    echo $stile->getId() . ' - ' . $stile->getNome() . "\n";
-}
-
 echo "\n=== SEGNALAZIONI DAL DATABASE ===\n";
 foreach ($pm->findAllSegnalazioni() as $segnalazione) {
     echo $segnalazione->getId() . ' - ' . $segnalazione->getDescrizione() . "\n";
---
+++ test_db_lika.php
@@ -0,0 +1,11 @@
+<?php
+require 'vendor/autoload.php';
+
+use InkMaster\Foundation\PersistentManager;
+
+$pm = PersistentManager::getInstance();
+
+echo "=== STILI DAL DATABASE ===\n";
+foreach ($pm->findAvailableStyles() as $stile) {
+    echo $stile->getId() . ' - ' . $stile->getNome() . "\n";
+}
